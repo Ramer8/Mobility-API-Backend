29741b5363d09973fa6cb93cb1e6e0fe576aca72
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-04 03:20:08 +0200

    Feat: user routes created

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express"
+import { auth } from "../middlewares/auth"
+import {
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController"
+
+export const userRouter = Router()
+
+// Profile
+// authRouter.post("/profile", auth, getUserProfile)
+userRouter.get("/users/profile", auth, getUserProfile)
+userRouter.put("/users/profile", auth, updateUserProfile)
+// authRouter.post("/auth/login", loginUser)

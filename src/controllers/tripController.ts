4b316cf611f3315b88213ae975ac8dc59f9ad518
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-07 16:43:29 +0200

    Feat: showMyTripsWithToken function and his route

--- src/controllers/tripController.ts
+++ src/controllers/tripController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { Trip } from "../database/models/Trip"
+import { User } from "../database/models/User"
 
 export const createTripWithToken = async (req: Request, res: Response) => {
   try {
@@ -30,64 +31,61 @@ export const createTripWithToken = async (req: Request, res: Response) => {
     })
   }
 }
-// export const showMytripsWithToken = async (
-//   req: Request,
-//   res: Response
-// ) => {
-//   try {
-//     const userId = req.tokenData.userId
-//     const user = await User.find({
-//       where: {
-//         id: userId,
-//       },
-//       select: {
-//         appointments: true,
-//         id: true,
-//         firstName: true,
-//         lastName: true,
-//       },
-//     })
-//     const appointment = await Appointment.find({
-//       order: {
-//         appointmentDate: "ASC",
-//       },
-//       where: {
-//         userId: userId,
-//       },
-//       relations: {
-//         service: true,
-//       },
-//       select: {
-//         appointmentDate: true,
-//         id: true,
-//         service: {
-//           serviceName: true,
-//           description: true,
-//           id: true,
-//         },
-//       },
-//     })
-//     if (!appointment) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "Appointment/s not found",
-//         error: Error,
-//       })
-//     }
-//     res.status(200).json({
-//       success: true,
-//       message: "Appointment retrieved successfuly",
-//       user,
-//       appointment,
-//     })
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Appointment can't be retriever successfully",
-//       error: error,
-//     })
-//   }
-// }
+export const showMyTripsWithToken = async (req: Request, res: Response) => {
+  try {
+    const userId = req.tokenData.userId
+    const user = await User.find({
+      where: {
+        id: userId,
+      },
+      select: {
+        trips: true,
+        id: true,
+        userName: true,
+        location: true,
+      },
+    })
+    const trip = await Trip.find({
+      order: {
+        tripDate: "ASC",
+      },
+      where: {
+        userId: userId,
+      },
+      relations: {
+        driver: true,
+      },
+      select: {
+        tripDate: true,
+        id: true,
+        driver: {
+          driverName: true,
+          carId: true,
+          id: true,
+        },
+      },
+    })
+    if (!trip) {
+      return res.status(404).json({
+        success: false,
+        message: "Trip/s not found",
+        error: Error,
+      })
+    }
+    res.status(200).json({
+      success: true,
+      message: "Trip retrieved successfuly",
+      user,
+      trip,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Trip can't be retriever successfully",
+      error: error,
+    })
+  }
+}
 
 // export const recoverAppointmentWithId = async (req: Request, res: Response) => {
 //   try {
@@ -165,33 +163,37 @@ export const createTripWithToken = async (req: Request, res: Response) => {
 //   })
 // }
 
-// export const deleteAppointmentById = async (req: Request, res: Response) => {
+// export const deleteTripById = async (req: Request, res: Response) => {
 //   try {
 //     const userId = req.tokenData.userId
-
-//     const appointmentToRemove: any = await Appointment.findOneBy({
+//     const tripToRemove: any = await Trip.findOneBy({
 //       userId: userId,
 //       id: parseInt(req.params.id),
 //     })
-
-//     if (!appointmentToRemove) {
+//     if (!tripToRemove) {
 //       return res.status(404).json({
 //         success: false,
-//         message: "Appointment can't be deleted because not exist in Data Base",
+//         message: "trip can't be deleted because not exist in Data Base",
 //       })
 //     }
+//     console.log(tripToRemove, "fdsagfadgdfa")
 
-//     const appointmentDeleted = await Appointment.delete(appointmentToRemove)
-
-//     res.status(200).json({
+//     const tripDeleted = await Trip.delete(tripToRemove)
+//     // if (!tripDeleted.affected) {
+//     //   return res.status(404).json({
+//     //     success: false,
+//     //     message: "trip can't be deleted",
+//     //   })
+//     // }
+//     return res.status(200).json({
 //       success: true,
-//       message: "Appointment deleted",
-//       appointmentDeleted: appointmentToRemove,
+//       message: "Trip deleted",
+//       tripDeleted: tripToRemove,
 //     })
 //   } catch (error) {
-//     res.status(500).json({
+//     return res.status(500).json({
 //       success: false,
-//       message: "Appointment can't be deleted",
+//       message: "Trip can't be deleted",
 //       error: error,
 //     })
 //   }
--- src/routes/tripRoutes.ts
+++ src/routes/tripRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express"
-import { createTripWithToken } from "../controllers/tripController"
+import {
+  createTripWithToken,
+  deleteTripById,
+  showMyTripsWithToken,
+} from "../controllers/tripController"
 import { auth } from "../middlewares/auth"
 
 export const tripRouter = Router()
@@ -7,7 +11,8 @@ export const tripRouter = Router()
 // Create trip:
 tripRouter.post("/trips", auth, createTripWithToken)
 
-// authRouter.post("/auth/login", loginUser)
+// tripRouter.delete("/trips/:id", auth, deleteTripById)
+tripRouter.get("/trips", auth, showMyTripsWithToken)
 // // Driver Authorization
 // authRouter.post("/auth/drivers/register", registerDriver)
 // authRouter.post("/auth/drivers/login", loginDriver)
